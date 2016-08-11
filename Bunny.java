6643dec9e7edd2128c443bba1bf302cf974744d2
kavinT1995 <kavin.tcse@ksrce.ac.in>
2016-08-11 15:01:20 +0530

    Create Bunny.java

---
+++ Bunny.java
@@ -0,0 +1,11 @@
+# Bunny
+public class Bunny {
+    
+	public static void main(String[] args) {
+		Scanner sr=new Scanner(System.in);
+		int n=sr.nextInt();
+		int ears=n+n;
+		System.out.println(ears);
+		
+}
+}
